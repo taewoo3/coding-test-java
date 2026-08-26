aec9054337b074e2ffcd7cf8bd594ab8be952fbe
taewoo3 <tw119kim@gmail.com>
2026-08-26 22:04:21 +0900

    260826

--- src/main/java/algomath/chap5/sec2/Prob2.java
+++ src/main/java/algomath/chap5/sec2/Prob2.java
@@ -11,11 +11,11 @@ public class Prob2 {
         for (int i=1; i<n+1;i++) {
             a[i] = sc.nextInt();
         }
-        int[] pattern = new int[n+1];
+        int[] pattern = new int[n+1]; // 방문 순서
         pattern[0] = 1;
-        int[] patternCount = new int[n+1];
+        int[] patternCount = new int[n+1]; // 마을 방문 횟수 count
         patternCount[1] = 1;
-        int[] cycleStartIdx = new int[n+1];
+        int[] cycleStartIdx = new int[n+1]; // 마을 방문 횟수 count가 2가 된 마을은 사이클이 생겼다 볼수있음
         cycleStartIdx[1] = 0;
         int[] cycleEndIdx = new int[n+1];
         int cycleStartNode = -1;
@@ -32,9 +32,11 @@ public class Prob2 {
                 break;
             }
         }
+        // 사이클 시작지점보다 k가 작으면 걍 k번째 방문기록을 패턴에서 바로 찾음
         if (k < cycleStartIdx[cycleStartNode]) {
             System.out.println(pattern[(int)k]);
         } else {
+            // (k-사이클 시작지점)%사이클 하면 방문 노드가 나옴
             int patternIdx = (int)((k-cycleStartIdx[cycleStartNode]) % (long)cycle);
             System.out.println(pattern[cycleStartIdx[cycleStartNode] + patternIdx]);
         }
